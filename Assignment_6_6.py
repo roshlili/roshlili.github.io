37699f13334565e9c2564631ea3f5a439def3fe1
Лиля <98236471+roshlili@users.noreply.github.com>
2022-04-03 17:22:57 +0300

    Add files via upload

---
+++ Assignment_6_6.py
@@ -0,0 +1,16 @@
+#l = [5,4,3,2,1]
+# #print(l)
+#print(list(reversed(l)))
+
+def my_reverse(l):
+    output = []
+    while len(l) > 0:
+        e = l[-1]
+        output.append(e)
+        l.pop(-1)
+    return(output)
+
+#l = [5,4,3,2,1]
+#print(my_reverse(l))
+print(my_reverse([5,4,3,2,1]))
+
